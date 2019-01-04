57ed661994ed3409ec63f4e2b63cd41d73ecc2be
Carl (Callie) du Preez <cdupreez@astutefse.com>
2019-01-04 11:53:22 +0200

    step 4 pass

--- KataCalculatorTests/UnitTest1.cs
+++ KataCalculatorTests/UnitTest1.cs
@@ -27,6 +27,15 @@ namespace KataCalculatorTests
             Assert.AreEqual(3, StringCalculator.Add("1,2"));
             Assert.AreEqual(8, StringCalculator.Add("3,5"));
         }
+
+        [TestMethod]
+        public void Step4_AnyAmountOfNumbersReturnsTheSum()
+        {
+            Assert.AreEqual(6, StringCalculator.Add("1,2,3"));
+            Assert.AreEqual(20, StringCalculator.Add("3,5,3,9"));
+        }
+        
+        
         
     }
 }
