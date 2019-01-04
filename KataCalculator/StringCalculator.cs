7072e45f1fe0fd90db938330b596cc22fa22010e
Carl (Callie) du Preez <cdupreez@astutefse.com>
2019-01-04 11:43:49 +0200

    step 3 pass

---
+++ KataCalculator/StringCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace KataCalculator
+
+{
+    public static class StringCalculator
+    {
+        public static void Main(string[] args)
+        {
+  
+        }
+
+        public static int Add(string str)
+        {
+            var number = 0;
+            
+            if (string.IsNullOrEmpty(str))
+            {
+                return number;
+            }
+            
+            // Split input string
+            var strNumbers = str.Split(',');
+            
+            if (strNumbers.Length >= 1)
+            {
+                foreach (var strNumber in strNumbers)
+                {
+                    number = number + int.Parse(strNumber);
+                }
+            }
+            
+           return number;
+        }
+    }
+}
--- KataCalculatorTests/UnitTest1.cs
+++ KataCalculatorTests/UnitTest1.cs
@@ -10,15 +10,22 @@ namespace KataCalculatorTests
         [TestMethod]
         public void Step1_ShouldTakeAStringAndReturnANumber()
         {
-            Assert.AreEqual(0, Program.Add(""));
+            Assert.AreEqual(0, StringCalculator.Add(""));
         }
 
         [TestMethod]
         public void Step2_ShouldTakeASingleNumberAndReturnThatNumber()
         {
             
-            Assert.AreEqual(1, Program.Add("1"));
-            Assert.AreEqual(3, Program.Add("3"));
+            Assert.AreEqual(1, StringCalculator.Add("1"));
+            Assert.AreEqual(3, StringCalculator.Add("3"));
+        }
+
+        [TestMethod]
+        public void Step3_TwoNumbersReturnSum()
+        {
+            Assert.AreEqual(3, StringCalculator.Add("1,2"));
+            Assert.AreEqual(8, StringCalculator.Add("3,5"));
         }
         
     }
